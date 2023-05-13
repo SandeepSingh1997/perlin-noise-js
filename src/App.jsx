ee4a5254bc0c6c74a7ceb583e4f3daadd6c44c09
SandeepSingh1997 <sandeepsingh10101997@gmail.com>
2023-05-13 11:23:23 +0530

    genrate random img with random color per pixel

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const canvas = document.querySelector("#my_canvas");
+    const width = canvas.getAttribute("width");
+    const height = canvas.getAttribute("height");
+    const ctx = canvas.getContext("2d");
+    const pixelSize = 2;
+    for (let x = 0; x < width; x += pixelSize) {
+      for (let y = 0; y < height; y += pixelSize) {
+        ctx.fillStyle = `#${randomNumber()}`;
+        drawPixel(ctx, x, y, pixelSize);
+      }
+    }
+    // ctx.clear();
+  }, []);
+
+  function randomNumber() {
+    return Math.floor(Math.random() * (255 * 255 * 255 - 1)).toString(16);
+  }
+
+  function drawPixel(ctx, posX, posY, size) {
+    ctx.fillRect(posX, posY, posX + size, posY + size);
+    ctx.fill();
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <canvas
+        id="my_canvas"
+        width="400"
+        height="400"
+        style={{ backgroundColor: "red" }}
+      ></canvas>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
